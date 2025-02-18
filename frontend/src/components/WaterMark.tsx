b928d4dcacc005ee5843b1158b31c815c8fb8dc7
zachczhang <309978130@qq.com>
2025-02-18 13:06:28 +0800

    init

--- frontend/src/components/WaterMark.tsx
+++ frontend/src/components/WaterMark.tsx
@@ -1,19 +1,19 @@
 import { Translator } from 'components/i18n';
 
-import 'assets/logo_dark.svg';
-import LogoDark from 'assets/logo_dark.svg?react';
-import 'assets/logo_light.svg';
-import LogoLight from 'assets/logo_light.svg?react';
+// import 'assets/logo_dark.svg';
+// import LogoDark from 'assets/logo_dark.svg?react';
+// import 'assets/logo_light.svg';
+// import LogoLight from 'assets/logo_light.svg?react';
 
-import { useTheme } from './ThemeProvider';
+// import { useTheme } from './ThemeProvider';
 
 export default function WaterMark() {
-  const { variant } = useTheme();
-  const Logo = variant === 'light' ? LogoLight : LogoDark;
+  // const { variant } = useTheme();
+  // const Logo = variant === 'light' ? LogoLight : LogoDark;
 
   return (
     <a
-      href="https://github.com/Chainlit/chainlit"
+      href="https://mdi.hkust-gz.edu.cn/"
       target="_blank"
       className="watermark"
       style={{
@@ -25,14 +25,6 @@ export default function WaterMark() {
       <div className="text-xs text-muted-foreground">
         <Translator path="chat.watermark" />
       </div>
-      <Logo
-        style={{
-          width: 65,
-          height: 'auto',
-          filter: 'grayscale(1)',
-          marginLeft: '4px'
-        }}
-      />
     </a>
   );
 }
